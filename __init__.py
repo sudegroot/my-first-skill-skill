e610699b112793bbc6f8d5ec2768a1a7b86ab766
Myrthe313 <Myrthe.sancta@gmail.com>
2021-01-05 20:18:09 +0000

    removed files

--- __init__.py
+++ __init__.py
@@ -5,8 +5,8 @@ class MyFirstSkill(MycroftSkill):
   def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('skill.first.my.intent')
-    def handle_skill_first_my(self, message):
+  @intent_file_handler('skill.first.my.intent')
+  def handle_skill_first_my(self, message):
         self.speak_dialog('skill.first.my')
 
 def create_skill():
