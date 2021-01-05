154d99e6ba8d572e741d4210489ca6a9e0b008cf
Marshmallows <Myrthe.sancta@gmail.com>
2021-01-05 21:21:43 +0100

    restored original file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-# Adding a comment
+
 class MyFirstSkill(MycroftSkill):
-  def __init__(self):
+    def __init__(self):
         MycroftSkill.__init__(self)
 
-  @intent_file_handler('skill.first.my.intent')
-  def handle_skill_first_my(self, message):
+    @intent_file_handler('skill.first.my.intent')
+    def handle_skill_first_my(self, message):
         self.speak_dialog('skill.first.my')
+
 
 def create_skill():
     return MyFirstSkill()
-
