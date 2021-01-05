d6d635eda68326a4a2a53dde6fc78f0b73d2331a
Marshmallows <Myrthe.sancta@gmail.com>
2021-01-05 21:44:05 +0100

    added study skill

--- __init__.py
+++ __init__.py
@@ -1,13 +1,17 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('skill.first.my.intent')
+    @intent_handler('skill.first.my.intent')
     def handle_skill_first_my(self, message):
         self.speak_dialog('skill.first.my')
+
+    @intent_handler('skill.study.intent')
+    def handle_skill_study(self, message):
+        self.speak_dialog('skill.study')
 
 
 def create_skill():
