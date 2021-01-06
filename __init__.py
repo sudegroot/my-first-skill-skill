ef6fa7b4c220a367a4b80a746ec1bc80d10cb59e
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-06 19:48:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,12 +6,7 @@ class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler('skill.first.my.intent')
-    def handle_skill_first_my(self, message):
-        self.speak_dialog('skill.first.my')
 
-        
-        
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
         time = extract_duration(self.get_response('skill.study'))[0]
