b6811c01945f01a71b3b24b605c9da64d2de7d4d
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-06 15:46:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,11 +15,12 @@ class MyFirstSkill(MycroftSkill):
         t = extract_duration(self.get_response('skill.study'))[0]
         self.speak_dialog('skill.study.confirmation', {'time': str(t)})
         duration = t*60
-        for i in range(duration):
-            time.sleep(1)
-        self.speak_dialog('skill.study.end')
+        return duration
+        #for i in range(duration):
+        #    time.sleep(1)
+        #self.speak_dialog('skill.study.end')
         
-             #   time = extract_duration(self.get_response('skill.study'))[0]
+      #   time = extract_duration(self.get_response('skill.study'))[0]
       #  self.speak_dialog('skill.study.confirmation', {'time': str(time)})
    
 
