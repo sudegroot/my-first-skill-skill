3864fffd2220c8bb52e75576425b6e9ef747cedd
AniekvanO <77107960+AniekvanO@users.noreply.github.com>
2021-01-07 14:12:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,31 @@
 #import time
 from mycroft import MycroftSkill, intent_handler
-from mycroft.util.parse import extract_duration
+from mycroft.messagebus import Message
 
 class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    #def initialize(self):
+        #self.add_event('skill.mycrofttimer.expired',
+                       #self.handler_time_up)
+
+    @intent_handler('skill.first.my.intent')
+    def handle_skill_first_my(self, message):
+        self.speak_dialog('skill.first.my')
 
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
-        time = extract_duration(self.get_response('skill.study'))[0]
-        self.speak_dialog('skill.study.confirmation', {'time': str(time)})
-        
-      #  t = extract_duration(self.get_response('skill.study'))[0]
-      #  self.speak_dialog('skill.study.confirmation', {'time': str(t)})
-      #  duration = t*60
-      #  time.sleep(int(duration))
-      #  self.speak_dialog('skill.study.end') 
-       
-     
-         
-   
+        time = self.get_response('skill.study')
+        # We receive the response from the user on how long they want to study for example '5 minutes"
+        # We use the message bus, where we put the message on the bus and format the string with 'set a timer'
+	# so that the Timer Skill from mycroft will recognize it and set the timer accordingly.
+        self.bus.emit(Message("recognizer_loop:utterance", {'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
+
+    #def handler_time_up(self, message):
+        #self.bus.emit(Message('speak',{"utterance": 'This is a test',
+        #"lang": 'en-us'}))
+
 
 def create_skill():
     return MyFirstSkill()
