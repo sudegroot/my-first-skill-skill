027d2fd632f6d156b3416a9e52d9e6fc4b997480
Marshmallows <Myrthe.sancta@gmail.com>
2021-01-07 17:58:16 +0100

    fixed a bug

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 #import time
 from mycroft import MycroftSkill, intent_handler
-from mycroft_bus_client import MessageBusClient, Message
+from mycroft.messagebus import Message
+#from mycroft_bus_client import MessageBusClient, Message
 class MyFirstSkill(MycroftSkill):
 
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def initialize(self):
-        client = MessageBusClient()
-        client.on('skill.mycrofttimer.expired', self.handle_skill_study_break)
-        client.run_forever()
+    # def initialize(self):
+    #     client = MessageBusClient()
+    #     client.on('skill.mycrofttimer.expired', self.handle_skill_study_break)
+    #     client.run_forever()
 
     @intent_handler('skill.first.my.intent')
     def handle_skill_first_my(self, message):
@@ -21,7 +22,7 @@ class MyFirstSkill(MycroftSkill):
         # We receive the response from the user on how long they want to study for example '5 minutes"
         # We use the message bus, where we put the message on the bus and format the string with 'set a timer'
         # so that the Timer Skill from mycroft will recognize it and set the timer accordingly.
-        client.emit(Message("recognizer_loop:utterance", data={'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
+        self.bus.emit(Message("recognizer_loop:utterance",{'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
 
     def handle_skill_study_break(message):
 #        self.get_response('skill.study.break')
