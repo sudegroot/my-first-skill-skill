d595193993f5f74d81f9572a07337a9f4004f947
Marshmallows <Myrthe.sancta@gmail.com>
2021-01-07 17:45:06 +0100

    try a timer function

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ class MyFirstSkill(MycroftSkill):
         # We receive the response from the user on how long they want to study for example '5 minutes"
         # We use the message bus, where we put the message on the bus and format the string with 'set a timer'
         # so that the Timer Skill from mycroft will recognize it and set the timer accordingly.
-        self.bus.emit(Message("recognizer_loop:utterance", {'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
+        client.emit(Message("recognizer_loop:utterance", data={'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
 
     def handle_skill_study_break(message):
 #        self.get_response('skill.study.break')
