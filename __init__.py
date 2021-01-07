2084c5101c899b22504a2bd224047567cddcf131
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-07 09:04:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,30 +5,22 @@ from mycroft.util.parse import extract_duration
 class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_handler('skill.first.my.intent')
-    def handle_skill_first_my(self, message):
-        self.speak_dialog('skill.first.my')
-
-        
+   
         
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
-        time = extract_duration(self.get_response('skill.study'))[0]
-        self.speak_dialog('skill.study.confirmation', {'time': str(time)})
-        
         blocks = extract_duration(self.get_response('skill.study'))[0]
-        self.speak_dialog('skill.study.confirmation', {'time': str(t)})
+        self.speak_dialog('skill.study.confirmation', {'time': str(blocks)})
         blocks = int(blocks)
         for i in range(blocks):
             time.sleep(1500)
             if i <  blocks-1:
-                self.speak_dialog('skill.study.break.begin')
+                self.speak_dialog('skill.breakbegin')
                 time.sleep(300)
-                self.speak_dialog('skill.study.break.end') 
+                self.speak_dialog('skill.breakend') 
             if i == blocks-1:
                 break
-        self.speak_dialog('skill.study.end') 
+        self.speak_dialog('skill.end') 
 
 
 
