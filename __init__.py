32091dbf6f26e20e94e689b0693fa4ef84acfd36
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-08 13:47:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,35 +1,30 @@
-#import time
+import time
 from mycroft import MycroftSkill, intent_handler
-from mycroft.messagebus import Message
+from mycroft.util.parse import extract_duration
 
 class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    #def initialize(self):
-        #self.add_event('skill.mycrofttimer.expired',
-                       #self.handler_time_up)
-
-    @intent_handler('skill.first.my.intent')
-    def handle_skill_first_my(self, message):
-        self.speak_dialog('skill.first.my')
-
+   
+        
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
-        time = self.get_response('skill.study')
-        # We receive the response from the user on how long they want to study for example '5 minutes"
-        # We use the message bus, where we put the message on the bus and format the string with 'set a timer'
-	# so that the Timer Skill from mycroft will recognize it and set the timer accordingly.
-        self.bus.emit(Message("recognizer_loop:utterance", {'utterances': ["set a timer for %s" %time], 'lang': 'en-us'}))
-
-
-    #def handler_time_up(self, message):
-        #self.bus.emit(Message('speak',{"utterance": 'This is a test',
-        #"lang": 'en-us'}))
-	
+        blocks = self.get_response('skill.study')
+        self.speak_dialog('skill.study.confirmation', {'time': str(blocks)})
+# To convert  blocks to an int, itt first needs to be a  string. The variable must be of time int for us to be able to put it into a range for the  loop
+        blocks = str(blocks)
+        blocks = int(blocks)
+        for i in range(blocks):
+            time.sleep(25)
+            if i <  blocks-1:
+                self.speak_dialog('skill.breakbegin')
+                time.sleep(5)
+                self.speak_dialog('skill.breakend') 
+            if i == blocks-1:
+                break
+        self.speak_dialog('skill.end') 
 
 
 
 def create_skill():
     return MyFirstSkill()
-
